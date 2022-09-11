7cd7b6c6cbc96ab72368f854e5078a6452318098
akashahmed-coder <ahmefakash3@gmail.com>
2022-09-11 15:04:44 +0600

    total rating div

--- client/src/components/SideRatingCount/SideRatingCount.js
+++ client/src/components/SideRatingCount/SideRatingCount.js
@@ -1,10 +1,42 @@
 import React from 'react'
 import './SideRatingCount.css'
+import { Rating } from '@mui/material';
 
 export default function SideRatingCount() {
   return (
     <div className='sideRatingCount'>
-        SideRatingCount
+        <div className='sideRatingCountTop'>
+           <h2 className='sideRatingCountTotal'>4.4</h2>
+           <Rating name="half-rating" defaultValue={4.4}  size="large" precision={0.1} readOnly/>
+           <p className='overAllReview'>(Overall 510 Review)</p>
+        </div>
+        <div className='sideRatingCountBottom'>
+          <div className='sideRatingCountBottomPerStar'>
+            <p>5 Star</p>
+            <Rating name="half-rating" defaultValue={5} style={{margin:"0px 10px"}}   size="medium"readOnly/>
+            <p>(200)</p>
+          </div>
+          <div className='sideRatingCountBottomPerStar'>
+            <p>4 Star</p>
+            <Rating name="half-rating" defaultValue={4} style={{margin:"0px 10px"}}   size="medium"readOnly/>
+            <p>(121)</p>
+          </div>
+          <div className='sideRatingCountBottomPerStar'>
+            <p>3 Star</p>
+            <Rating name="half-rating" defaultValue={3} style={{margin:"0px 10px"}} size="medium"readOnly/>
+            <p>(50)</p>
+          </div>
+          <div className='sideRatingCountBottomPerStar'>
+            <p>2 Star</p>
+            <Rating name="half-rating" defaultValue={2} style={{margin:"0px 10px"}}  size="medium"readOnly/>
+            <p>(10)</p>
+          </div>
+          <div className='sideRatingCountBottomPerStar'>
+            <p>1 Star</p>
+            <Rating name="half-rating" defaultValue={1} style={{margin:"0px 10px"}}  size="medium"readOnly/>
+            <p>(8)</p>
+          </div>
+        </div>
     </div>
   )
 }
