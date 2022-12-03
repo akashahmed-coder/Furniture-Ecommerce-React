13bb53ac3f823b8e546111b1f1c19c65d05f0fd3
Akash Ahmed <ahmefakash3@gmail.com>
2022-12-03 18:47:58 +0600

    responsive

--- client/src/components/reactSlider/ReactSlider.js
+++ client/src/components/reactSlider/ReactSlider.js
@@ -21,35 +21,49 @@ export default class Responsive extends Component {
     var settings = {
       infinite: true,
       speed: 500,
-      slidesToShow: 3,
+      slidesToShow: 6,
       slidesToScroll: 2,
       initialSlide: 0,
       autoplay: true,
       autoplaySpeed: 2000,
       responsive: [
         {
-          breakpoint: 1024,
+          breakpoint: 1120,
           settings: {
-            slidesToShow: 2,
+            slidesToShow: 5,
             slidesToScroll: 1,
             infinite: true,
           }
         },
         {
-          breakpoint: 600,
+          breakpoint: 900,
           settings: {
-            slidesToShow: 1,
+            slidesToShow: 4,
             slidesToScroll: 1,
             initialSlide: 2
           }
         },
         {
-          breakpoint: 480,
+          breakpoint: 740,
           settings: {
-            slidesToShow: 1,
+            slidesToShow: 3,
             slidesToScroll: 1
           }
-        }
+        },
+        {
+          breakpoint: 555,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
       ]
     };
     return (
--- client/src/components/trendingFurniture/TrendingFurniture.js
+++ client/src/components/trendingFurniture/TrendingFurniture.js
@@ -2,15 +2,6 @@ import React from 'react'
 import ReactSlider from '../reactSlider/ReactSlider';
 
 export default function TrendingFurniture() {
-    const slides = [
-        {image:"https://picsum.photos/200/300",title:"This is a title",description:"This is a description"},
-        {image:"https://picsum.photos/600/500",title:"This is a second title",description:"This is a second description"},
-        {image:"https://picsum.photos/700/600",title:"This is a third title",description:"This is a third description"},
-        {image:"https://picsum.photos/500/400",title:"This is a fourth title",description:"This is a fourth description"},
-        {image:"https://picsum.photos/200/300",title:"This is a fifth title",description:"This is a fifth description"},
-        {image:"https://picsum.photos/800/700",title:"This is a sixth title",description:"This is a sixth description"},
-        {image:"https://picsum.photos/300/400",title:"This is a seventh title",description:"This is a seventh description"},
-    ]
   return (
    <div className='container'>
      <div className='trendingFurniture'>
