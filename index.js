1dc5b86997d720fe77e4424007dc3c92e9e3288f
akashahmed-coder <ahmefakash3@gmail.com>
2023-01-13 01:50:35 +0600

    build

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const path = require('path')
 const app = express()
 app.use(express.json())
 const PORT = process.env.PORT || 5000
-    app.use(express.static('client/build'))
-    app.get("*",(req,res)=>{
-        res.sendFile(path.join(__dirname,'client','build','index.html'))
-    })
+app.use(express.static('client/build'))
+app.get("*",(req,res)=>{
+       res.sendFile(path.join(__dirname,'client','build','index.html'))
+})
 app.listen(PORT,()=>{
     console.log(`server runnig successfull at ${PORT}`)
 })
