8f2d386e6e0068beb81988e5ffd5fc474b715fe5
akashahmed-coder <ahmefakash3@gmail.com>
2023-01-15 11:03:39 +0600

    responsive navbar

--- client/src/components/mainpage/navber/Navber.js
+++ client/src/components/mainpage/navber/Navber.js
@@ -2,15 +2,21 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Logo1 from '../../../img/logo-1.webp'
 import Cart from '../../../img/cart.svg.png'
+import CloseIcon from '@mui/icons-material/Close';
+import MenuIcon from '@mui/icons-material/Menu';
 
 export default function Navber() {
   return (
     
         <div className='container'>
           <div className='navbar'>
+          <div className='menu'>
+             <MenuIcon htmlColor='white'fontSize='large'/>
+          </div>
           <div className='logo'>
            <img src={Logo1} alt='header'/>
            </div>
+
            <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About Us</Link></li>
@@ -19,13 +25,20 @@ export default function Navber() {
                 <li><Link to="/contact">Contact Us</Link></li>
                 <li>
                       <div className="cart-icon">
-                      <span>3</span>
+                      <span className='cartCount'>3</span>
                       <Link to="/cart">
                       <img src={Cart} alt='' width='30' />
                       </Link>
                     </div>
                 </li>
+                <li > <CloseIcon htmlColor='white'fontSize='large' className='menu'/></li>
               </ul>
+              <div className="SmallScreenCart" style={{display:"none"}}>
+                      <span className='cartCount'>3</span>
+                      <Link to="/cart">
+                      <img src={Cart} alt='' width='30' />
+                      </Link>
+              </div>
           </div>
         </div>
    
